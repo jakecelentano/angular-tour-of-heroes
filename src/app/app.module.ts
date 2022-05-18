fe5380a7462032adc4ba58b4b7d9b798b245be42
Jake Celentano <jakecelentano@gmail.com>
2022-05-18 15:47:38 -0400

    Revert "styling on hero page"

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,18 +2,13 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
-import { HeroesComponent } from './heroes/heroes.component';
-
-import { FormsModule } from '@angular/forms'; // <-- NgModel lives here
 
 @NgModule({
   declarations: [
-    AppComponent,
-    HeroesComponent
+    AppComponent
   ],
   imports: [
-    BrowserModule,
-    FormsModule
+    BrowserModule
   ],
   providers: [],
   bootstrap: [AppComponent]
--- src/app/hero.ts
+++
@@ -1,7 +0,0 @@
-export interface Hero {
-  id: number;
-  name: string;
-  nickname?: string;
-  race?: string;
-  class?: string;
-}
